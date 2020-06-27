4f80701fb3e97643bfeb9148b46ff608b6e9cb96
Noor Schat <nschat@student.codam.nl>
2020-06-27 15:29:15 +0200

    fix: remove unneeded color unpacking functions

--- src/utils/color.c
+++ src/utils/color.c
@@ -6,31 +6,11 @@
 /*   By: nschat <nschat@student.codam.nl>             +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/06/26 15:17:16 by nschat        #+#    #+#                 */
-/*   Updated: 2020/06/26 15:45:54 by nschat        ########   odam.nl         */
+/*   Updated: 2020/06/27 15:29:10 by nschat        ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 int	pack_trgb(int t, int r, int g, int b)
 {
 	return (t << 24 | r << 16 | g << 8 | b);
-}
-
-int	unpack_t(int trgb)
-{
-	return (trgb & 0xFF);
-}
-
-int	unpack_r(int trgb)
-{
-	return (trgb & 0xFF00);
-}
-
-int	unpack_g(int trgb)
-{
-	return (trgb & 0xFF0000);
-}
-
-int	unpack_b(int trgb)
-{
-	return (trgb & 0xFF000000);
 }
