d2de132bac4649b1ce3dbf06556ca997e68eb233
Norah Hannah Schat <nschat@student.codam.nl>
2020-09-15 20:32:25 +0200

    feat: add get_ident function

--- src/parser/parse.c
+++ src/parser/parse.c
@@ -6,19 +6,86 @@
 /*   By: nschat <nschat@student.codam.nl>             +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/05/27 13:09:32 by nschat        #+#    #+#                 */
-/*   Updated: 2020/07/16 18:33:07 by nschat        ########   odam.nl         */
+/*   Updated: 2020/09/15 20:30:48 by nschat        ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "renderer.h"
 #include "libft.h"
+#include "error.h"
 #include <stdlib.h>
 
+int				ft_strcmp(const char *s1, const char *s2)
+{
+	while (*s1 == *s2)
+	{
+		if (*s1 == '\0')
+			break ;
+		s2++;
+		s1++;
+	}
+	return (*(unsigned char *)s1 - *(unsigned char *)s2);
+}
+
+static int		valid_ident(char *ident)
+{
+	int	ret;
+
+	ret = 0;
+	if (ft_strcmp("R", ident))
+		ret = 1;
+	if (ft_strcmp("A", ident))
+		ret = 1;
+	if (ft_strcmp("c", ident))
+		ret = 1;
+	if (ft_strcmp("l", ident))
+		ret = 1;
+	if (ft_strcmp("sp", ident))
+		ret = 1;
+	if (ft_strcmp("pl", ident))
+		ret = 1;
+	if (ft_strcmp("cy", ident))
+		ret = 1;
+	if (ft_strcmp("tr", ident))
+		ret = 1;
+	return (ret);
+}
+
+static size_t	ft_strlen_space(const char *s)
+{
+	size_t	len;
+
+	len = 0;
+	while (s[len] != '\0' && !ft_isspace(s[len]))
+		len++;
+	return (len);
+}
+
+char		*get_identifier(char *s) // R,A,c,l,sp,pl,cy,tr
+{
+	char	*ident;
+	size_t	count;
+	size_t	i;
+
+	count = ft_strlen_space(s);
+	ident = malloc(sizeof(char) * (count + 1));
+	i = 0;
+	while (i < count)
+	{
+		ident[i] = s[i];
+		i++;
+	}
+	ident[i] = '\0';
+	if (!valid_ident(ident))
+	{
+		puterror("parse: invalid identifier");
+		free(ident);
+		return (NULL);
+	}
+	return (ident);
+}
+
 /*
- *char		*get_identifier(char *s) // R,A,c,l,sp,pl,cy,tr
- *{
- *}
- *
  *t_vec		get_vector(char *s)
  *{
  *}
@@ -38,9 +105,15 @@
  *double		get_double(char *s)
  *{
  *}
- *
+ */
+
+/*
  *char		**get_fields(char *line) // use above functions depending on identifier
  *{
+ *    char	*ident;
+ *
+ *    ident = get_identifier(line);
+ *    if (ft_strcmp(ident, "R") == 0)
  *}
  */
 
